dc4b969737165cd6557257eb6cb17e4256e1dcea
Dominic Tremblay <dominictremblay24@gmail.com>
2016-05-24 15:58:51 +0000

    Exercise step 6-validation errors

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '23669d47b0d5237d19de30dbadff04aeee353d8f7835b4bae85b2d722129d6b564d090974acdcc43d7a6f1880f3a50b2e664e955a686637d4916ce2d88fd8e20'
